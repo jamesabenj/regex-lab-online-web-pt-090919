e960e5acd06ead2957c76b6079e7df3e10155dfa
James Benjamin <james.abenj@gmail.com>
2019-09-27 13:43:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,14 @@
 def starts_with_a_vowel?(word)
+  word.scan(/^[aeiou]/).any?
+ end 
 
-end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/^[un]$[ing]/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -15,5 +16,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+phone.scan(/(\d{3}\d{3}\d{4}|\d{3}-\d{3}-\d{4})/.any?
 end
