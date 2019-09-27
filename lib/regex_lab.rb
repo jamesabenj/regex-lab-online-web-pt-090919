a38c440cf8ec719c25bba7345d741dc2ed036429
James Benjamin <james.abenj@gmail.com>
2019-09-27 14:13:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,11 +4,11 @@ def starts_with_a_vowel?(word)
 
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/^un\w+ing/)
+text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-text.scan(/^\w{5}\z/)
+text.scan(/\w{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
