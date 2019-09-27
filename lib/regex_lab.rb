9c010ecc770ddc88e594a30439141715becb06a4
James Benjamin <james.abenj@gmail.com>
2019-09-27 13:53:55 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,7 @@ def starts_with_a_vowel?(word)
 
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/^[un][ing]$/)
+text.scan(/^[un]\w+[ing]$/)
 end
 
 def words_five_letters_long(text)
