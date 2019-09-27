6176b1da648cab6909cbd91ad3f95fb9a3648646
James Benjamin <james.abenj@gmail.com>
2019-09-27 13:46:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,7 @@ def starts_with_a_vowel?(word)
 
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/^[un]$[ing]/)
+text.scan(/^[un][ing]$/)
 end
 
 def words_five_letters_long(text)
@@ -16,5 +16,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-phone.scan(/(\d{3}\d{3}\d{4}|\d{3}-\d{3}-\d{4})/.any?
+
 end
